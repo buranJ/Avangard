e502774784777b43994fb11d719221f2335a6e52
Buran <125240291+buranJ@users.noreply.github.com>
2025-03-16 11:47:44 -0700

    Delete src directory

--- src/store/api/products/index.ts
+++
@@ -1,9 +0,0 @@
-import { productsApi } from './products.api';
-
-export const products = {
-  middleware: [productsApi.middleware],
-  reducers: {
-    [productsApi.reducerPath]: productsApi.reducer
-  },
-  slices: {}
-};
